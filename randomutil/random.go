f156d6e32fb802b27f08a38841c2a7c0a73c85f8
wangjun <wangjun@whdrawing.com>
2022-01-24 14:24:53 +0800

    init

--- randomutil/random.go
+++ randomutil/random.go
@@ -6,15 +6,26 @@ import (
 	"time"
 )
 
-func GetRandomString(count int) string {
-	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	bytes := []byte(str)
+func GetRandomCharacter(targetStr string, count int) string {
+	bytes := []byte(targetStr)
 	var result []byte
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < count; i++ {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+// pseudo-random string
+func GetRandomString(count int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	return GetRandomCharacter(str, count)
+}
+
+// pseudo-random numeric
+func GetRandomNumeric(count int) string {
+	str := "0123456789"
+	return GetRandomCharacter(str, count)
 }
 
 // pseudo-random number in [min,max)
